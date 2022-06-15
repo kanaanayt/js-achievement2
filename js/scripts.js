ab88caa2126fbebcd4055499e4ce2a8d6ddf3086
kanaanayt <kanaanabukhadra@gmail.com>
2022-06-15 20:57:54 +0300

    1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-alert('Hello World');
-let favoriteFood = 'Pasta';
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList[0] = 'hello';
+console.log(pokemonList[0]);
+pokemonList = [
+  { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
+  { name: 'Jigglypuff', height: 5, types: ['fairy', 'normal'] },
+  { name: 'Magnemite', height: 3, types: [electric, steel]}
+]
