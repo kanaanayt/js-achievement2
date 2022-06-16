18a57126f90d233a7ee325c9bf526c7b564227cb
kanaanayt <kanaanabukhadra@gmail.com>
2022-06-16 20:56:30 +0300

    1.3 done

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,15 @@ pokemonList = [
   { name: 'Jigglypuff', height: 5, types: ['fairy', 'normal'] },
   { name: 'Magnemite', height: 3, types: ['electric', 'steel']}
 ]
+
+for (let index = 0; index < pokemonList.length; index++)
+{
+  let message = "";
+  if (pokemonList[index].height > 6)
+  {
+    message = " - Wow that's big!";
+  }
+  document.write("<p>" + "Name: " + pokemonList[index].name + "</p>");
+  document.write("<p>" + "Height: " + pokemonList[index].height + message + "</p>");
+  document.write("<p>" + "Types: " + pokemonList[index].types[0] + " and " + pokemonList[index].types[1] +  "</p>");
+};
