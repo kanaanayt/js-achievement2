469bc28baf2ea2b8d22a97e683b1c81e52c78097
kanaanayt <kanaanabukhadra@gmail.com>
2022-06-16 20:35:19 +0300

    fixed quotes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 let pokemonList = [];
-pokemonList[0] = 'hello';
-console.log(pokemonList[0]);
 pokemonList = [
   { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
   { name: 'Jigglypuff', height: 5, types: ['fairy', 'normal'] },
-  { name: 'Magnemite', height: 3, types: [electric, steel]}
+  { name: 'Magnemite', height: 3, types: ['electric', 'steel']}
 ]
