d7ee84b040f61d9ab28635242225e0eda7d49495
kanaanayt <kanaanabukhadra@gmail.com>
2022-06-20 20:14:18 +0300

    IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,39 @@ pokemonList = [
   { name: 'Magnemite', height: 3, types: ['electric', 'steel']}
 ]
 
-for (let index = 0; index < pokemonList.length; index++)
+
+let pokemonRepository = (function ()
 {
-  let message = "";
-  if (pokemonList[index].height > 6)
+  let pokemonListIIFE = [];
+  function add(pokemon)
   {
-    message = " - Wow that's big!";
+    pokemonListIIFE.push(pokemon);
   }
-  document.write("<p>" + "Name: " + pokemonList[index].name + "</p>");
-  document.write("<p>" + "Height: " + pokemonList[index].height + message + "</p>");
-  document.write("<p>" + "Types: " + pokemonList[index].types[0] + " and " + pokemonList[index].types[1] +  "</p>");
-};
+  function getAll()
+  {
+    return pokemonListIIFE;
+  }
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonList.forEach(function(pokemon)
+{
+  pokemonRepository.add(pokemon);
+});
+
+console.log(pokemonRepository.getAll());
+
+// for (let index = 0; index < pokemonList.length; index++)
+// {
+//   let message = "";
+//   if (pokemonList[index].height > 6)
+//   {
+//     message = " - Wow that's big!";
+//   }
+//   document.write("<p>" + "Name: " + pokemonList[index].name + "</p>");
+//   document.write("<p>" + "Height: " + pokemonList[index].height + message + "</p>");
+//   document.write("<p>" + "Types: " + pokemonList[index].types[0] + " and " + pokemonList[index].types[1] +  "</p>");
+// };
